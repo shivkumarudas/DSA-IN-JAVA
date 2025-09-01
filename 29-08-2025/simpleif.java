6a5ad28ad80995e7d67b5fe5d281809f95368cb1
shivkumar udas <140606497+shivkumarudas@users.noreply.github.com>
2025-09-01 21:17:13 +0530

    30-08-2025

---
+++ 29-08-2025/simpleif.java
@@ -0,0 +1,8 @@
+public class simpleif {
+    public static void main (String[] args){
+        int a=5;
+        if(a>10){
+            System.out.println("a is greater than 10");
+        }
+    }      
+}
